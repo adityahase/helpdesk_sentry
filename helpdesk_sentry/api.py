c09c607f348dbfb82c2aa9b8a3e25a801d9ccc6c
Aditya Hase <aditya@adityahase.com>
2024-11-07 18:51:48 +0530

    feat: Add whitelisted method to accept sentry webhooks

---
+++ helpdesk_sentry/api.py
@@ -0,0 +1,21 @@
+# Copyright (c) 2024, Frappe and contributors
+# See license.txt
+
+import frappe
+
+
+@frappe.whitelist(allow_guest=True, xss_safe=True)
+def hook(*args, **kwargs):
+	webhook = frappe.get_doc(
+		{
+			"doctype": "Sentry Webhook Log",
+			"payload": frappe.request.get_data(as_text=True),
+		}
+	)
+	try:
+		webhook.insert(ignore_permissions=True)
+		frappe.db.commit()
+	except Exception as e:
+		frappe.log_error("Sentry Webhook Insert Error")
+		raise e
+
